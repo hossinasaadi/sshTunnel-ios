288f98c46172e1288a17e8bdab353561a63e5db3
hossinasaadi <hossin.asaadi@gmail.com>
2023-07-23 15:46:51 +0400

    Initial Commit

---
+++ sshTunnel/sshTunnelApp.swift
@@ -0,0 +1,17 @@
+//
+//  sshTunnelApp.swift
+//  sshTunnel
+//
+//  Created by HossinAsaadi on 7/23/23.
+//
+
+import SwiftUI
+
+@main
+struct sshTunnelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
